fc92b14ace35ef4d4d7ce671cb4ebb28ad85c3cc
Sylvie-B <unsolo@hotmail.fr>
2020-12-15 19:37:56 +0100

    get paragraph console substring chaine en tableau

--- exo.js
+++ exo.js
@@ -1,3 +1,32 @@
 let pomme = "Elle est belle ma pomme !";
 
+let paraIdxPom = document.getElementById("index-pomme");
+
+paraIdxPom.innerHTML = pomme.indexOf("pomme");
+
+let lastIdxM = document.getElementById("last-index-m");
+
+lastIdxM.innerHTML = pomme.lastIndexOf('m');
 console.log(pomme.lastIndexOf('m'));
+
+
+if(pomme.startsWith('Elle')){
+    console.log('Elle est belle ma pomme ! Commence par Elle');
+}
+// exo 4
+if(pomme.endsWith('!')){
+    console.log('Elle est belle ma pomme ! Se termine par un point d\'exclamation');
+}
+
+// exo 5
+let paraPom = document.getElementById('pomme');
+let newPom = pomme.substring(18, 23);
+
+paraPom.innerHTML = newPom;
+
+// exo 6
+let oneLine = document.getElementById("one-line");
+
+for (let i = 0 ; i < pomme.length ; i++){
+    oneLine.innerHTML += (pomme[i] + "<br>");
+}
